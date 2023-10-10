4d49980f6ae7391f5ba342c9c9dc1c3eea9655b8
Bayram Akın <bayram.akn.2000@gmail.com>
2023-10-10 21:50:09 +0300

    Create ODEV_2.sql

---
+++ ODEV_2.sql
@@ -0,0 +1,9 @@
+--1.Answer
+select * from film
+where replacement_cost between 12.99 and 16.98;
+--2.Answer 
+select first_name,last_name from actor
+where first_name in ('Penelope', 'Nick' , 'Ed') ;
+--3.Answer
+select * from film
+where rental_rate in (0.99,2.99,4.99) and replacement_cost in (12.99,15.99,28,99);
